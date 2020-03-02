3d31ba16ab2c4749cf56be9a878557f6ab662dea
bernie <xiongyun100@163.com>
2020-03-02 16:21:40 +0800

    J.U.C并发容器

---
+++ concurrency/src/main/java/com/bernie/concurrency/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,58 @@
+package com.bernie.concurrency.example.concurrent;
+
+import com.bernie.concurrency.annotations.ThreadSafe;
+import com.bernie.concurrency.annotations.ThreadUnsafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * ConcurrentHashMap
+ *
+ * @Description ConcurrentHashMap是线程安全对象
+ * @Author Bernie【xiongyun100@163.com】
+ * @Date 2020/2/28
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentHashMapExample {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发的执行线程数
+    public static int threadTotal = 200;
+
+    private static Map<Integer,Integer> map = new ConcurrentHashMap<>();
+    public static void main(String[] args)throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i=0;i<clientTotal;i++){
+            final int count = i;
+            executorService.execute(()->{
+                try{
+                    //信号量判断当前并发的线程是否有200个，有的话，不返回结果，也不执行后面的add方法
+                    semaphore.acquire();
+                    update(count);
+                    //在当前并发范围内执行了结果，信号量放行一个线程
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception:{}",e);
+                }
+                //5000个线程请求完成一个的时候，就减一
+                countDownLatch.countDown();
+            });
+        }
+        //当所有请求5000个都执行完成后。执行await后面的逻辑
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("map size is:{}",map.size());
+    }
+
+    private static void update(int i){
+        map.put(i,i);
+    }
+}
---
+++ concurrency/src/main/java/com/bernie/concurrency/example/concurrent/CopyOnWriteArraySetExample.java
@@ -0,0 +1,55 @@
+package com.bernie.concurrency.example.concurrent;
+
+import com.bernie.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * CopyOnWriteArraySet
+ *
+ * @Description CopyOnWriteArraySet是线程安全的，在写操作会加锁。
+ * @Author Bernie【xiongyun100@163.com】
+ * @Date 2020/2/28
+ */
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArraySetExample {
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发的执行线程数
+    public static int threadTotal = 200;
+
+    private static Set<Integer> set = new CopyOnWriteArraySet<>();
+    public static void main(String[] args)throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i=0;i<clientTotal;i++){
+            final int count = i;
+            executorService.execute(()->{
+                try{
+                    //信号量判断当前并发的线程是否有200个，有的话，不返回结果，也不执行后面的add方法
+                    semaphore.acquire();
+                    update(count);
+                    //在当前并发范围内执行了结果，信号量放行一个线程
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception:{}",e);
+                }
+                //5000个线程请求完成一个的时候，就减一
+                countDownLatch.countDown();
+            });
+        }
+        //当所有请求5000个都执行完成后。执行await后面的逻辑
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("set size is:{}", set.size());
+    }
+
+    private static void update(int i){
+        set.add(i);
+    }
+}
