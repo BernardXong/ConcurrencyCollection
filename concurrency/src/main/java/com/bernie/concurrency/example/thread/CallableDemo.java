442b46ce4f2c02153131b602f5f9374a043258ab
bernie <xiongyun100@163.com>
2020-03-04 17:59:50 +0800

    线程执行方式

---
+++ concurrency/src/main/java/com/bernie/concurrency/example/thread/CallableDemo.java
@@ -0,0 +1,11 @@
+package com.bernie.concurrency.example.thread;
+
+/**
+ * @Author: Bernie
+ * @CreateTime: 2020-03-04 17:12
+ * @Description:
+ * @Email: xiongyun100@163.com
+ */
+
+public class CallableDemo {
+}
